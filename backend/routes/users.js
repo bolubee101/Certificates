704542d61ce6d23d8aa876e7f4a7191ba6133da8
Bolubee101 <akinwandeakinboluwarin@gmail.com>
2020-08-29 12:36:20 +0100

    push

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/users");
-
+router.use(express.json());
 router.post("/seed", (req, res) => {
   let Users=req.body;
   User.collection.insertMany(Users,(err,docs)=>{
