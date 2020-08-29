d9f6af4198ad6b4208f0eabe009fb120325f22d3
Bolubee101 <akinwandeakinboluwarin@gmail.com>
2020-08-29 20:09:39 +0100

    commit

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -4,9 +4,12 @@ const User = require("../models/users");
 router.use(express.json());
 
 router.post("/seed", (req, res) => {
-  let Users = req.body.data;
+  let Users = req.body.data
   User.collection.insertMany(Users, (err, docs) => {
-    if (err) throw err;
+    if (err) {
+      res.send("Omo, error de");
+      throw err;
+    }
     else console.log(docs.length)
     res.send("done");
   })
