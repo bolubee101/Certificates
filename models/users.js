b8a336354f8d7687bd79ed8ea3fadc553772672d
Bolubee101 <akinwandeakinboluwarin@gmail.com>
2020-08-29 23:49:30 +0100

    push

--- models/users.js
+++ models/users.js
@@ -4,11 +4,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  "First Name": {
-    type: String,
-    required: true
-  },
-  "Last Name": {
+  "name": {
     type: String,
     required: true
   },
