65c48a76b890f12b082efa70a950ea7f8ad7d176
Bolubee101 <akinwandeakinboluwarin@gmail.com>
2020-08-29 13:55:11 +0100

    push

--- backend/views/js/okay.js
+++ backend/views/js/okay.js
@@ -8,12 +8,22 @@ let main = () => {
       data[input.name] = input.value;
     }
   }
-  let email=data["email"]
-if (email==""){
-  alert("Input a valid email address")
-}else{
-  //document.write(name);
-  console.log(email)
-  location.href=`https://fierce-bastion-56296.herokuapp.com/EmailCheck/?email=${email}`
-}
+  let email = data["email"]
+  if (email == "") {
+    alert("Input a valid email address")
+  } else {
+    //document.write(name);
+    if (ValidateEmail(email)) {
+      console.log(email)
+      location.href = `https://fierce-bastion-56296.herokuapp.com/EmailCheck/?email=${email}`
+    }
+  }
 };
+
+function ValidateEmail(email) {
+  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+    return (true)
+  }
+  alert("You have entered an invalid email address!")
+  return (false)
+}
