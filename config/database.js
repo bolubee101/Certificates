8c762e089ad4422c57f900a422fefe3cdcc813f0
Bolubee101 <akinwandeakinboluwarin@gmail.com>
2020-09-01 12:04:23 +0100

    push

--- config/database.js
+++ config/database.js
@@ -1,3 +1,3 @@
 module.exports={
-    database:"mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=false"
+    database:"mongodb+srv://geek:rJ7fWC05urlZwNQ7@cluster0.prrwn.mongodb.net/dsc?retryWrites=true&w=majority"
 }
